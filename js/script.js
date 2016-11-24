0c3155764fe47dd9c3b5e61c6e2563f948fa3a68
arulk8 <arulmurugan194@gmail.com>
2016-11-24 15:08:06 +0530

    changed return

--- js/script.js
+++ js/script.js
@@ -20,9 +20,9 @@ function loadData() {
     // YOUR CODE GOES HERE!
     var googlemapurl = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location='+ addr +'&key=AIzaSyArpgp1MhKgPjXnjFCM4Fk6XoPycFsPSKc';
    $body.append('<img class="bgimg" src= "'+ googlemapurl +'">');
-    return false;
+   
 
-var nyturl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+ cities +'&sort=newest&api-key=af6a39a346a54184bd8ba93317663eb0&fl=headline,snippet,web_url';
+var nyturl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+ cities +'&sort=newest&api-' + 'key=af6a39a346a54184bd8ba93317663eb0&fl=headline,snippet,web_url';
 
 $.getJSON( nyturl, function( data ) {
   $nytHeaderElem.text('New York Times Article About' + cities);
@@ -32,9 +32,10 @@ $.getJSON( nyturl, function( data ) {
   {
     var article =articles[i];
    $nytElem.append('<li class="article">'+'<a href="'+ article.Web_url +'">'+article.headline.main+'</a>'+'<p>'+ article.snippet +'<p>'+'</li>');
-  }
+  };
  
 
 });
+ return false;
 }
 $('#form-container').submit(loadData);
